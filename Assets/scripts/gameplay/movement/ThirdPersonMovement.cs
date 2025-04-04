30f9ad68e7d5d18fcd7d4c560a18cf48f7d4a4e6
tydino <tydino909@gmail.com>
2025-04-04 19:28:43 -0400

    added some function to the 3rd person controller
    you can now sprint and jump. not much more, though i did fix a visual bug that the cammera had when walking.

--- Assets/scripts/gameplay/movement/ThirdPersonMovement.cs
+++ Assets/scripts/gameplay/movement/ThirdPersonMovement.cs
@@ -6,15 +6,72 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Rigidbody rb;
+    [Header("speeds")]
+    public float walkSpeed = 6f;
+    public float runSpeed = 10f;
+    public float airSpeed = 8f;
     public float speed = 6f;
+
+    [Header("Jumping")]
+    public float jumpForce;
+    public float jumpCooldown;
+    bool readyToJump;
+    [Header("turning")]
     public float turnSmoothTime = 0.5f;
     float turnSmoothVelocity;
+    [Header("KeyBinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public MoveMentState state;
+    public enum MoveMentState
+    {
+        walking,
+        sprinting,
+        air
+    }
+    [Header("ground Check")]
+    public float playerHeight;
+    public LayerMask whatIGround;
+    bool grounded;
 
-    public float hor;
-    public float ver;
+    void Start(){readyToJump=true;}
+
+    void checkKeys()
+    {
+        if (Input.GetKeyDown(sprintKey)==true)
+        {
+            state=MoveMentState.sprinting;
+        }else
+        {
+            state=MoveMentState.walking;
+        }
+    }
 
     void Update()
     {
+        checkKeys();
+        
+        //jump
+        if(Input.GetKey(jumpKey) && readyToJump == true && grounded == true)
+        {
+            readyToJump = false;
+
+            jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+        //grounded?
+        grounded=Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f+0.2f, whatIGround);
+        if (grounded == false){state=MoveMentState.air;}
+
+        //main movement
+        if(state==MoveMentState.walking){speed = walkSpeed;}
+        if(state==MoveMentState.sprinting){speed = runSpeed;}
+        if(state==MoveMentState.air){speed = airSpeed;}
+
+        //get direction
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -28,7 +85,17 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir * speed * Time.deltaTime);
         }
-        hor=horizontal;
-        ver=vertical;
+    }
+
+    void jump()
+    {
+        //reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    void ResetJump()
+    {
+        readyToJump=true;
     }
 }
