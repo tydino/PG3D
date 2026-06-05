7ee94621f76b847e14f4bca29121b390ce12b575
tydino <tydino909@gmail.com>
2026-06-05 15:37:50 -0400

    testing complete
    I will now be implementing them when I feel like it as my testing was strenous... (It all was horrible... nothing worked properly I will submit this for now)

--- Assets/scripts/gameplay/NPC/NPC.cs
+++ Assets/scripts/gameplay/NPC/NPC.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    public Types type;
     public float WalkSpeed = 0.5f;
     public float FollowRange;
     public float AttackDamage;
     public float MaxHealth = 1f;
     public float JumpStrength = 1f;
+
+    public enum Types
+    {
+        Hostile,
+        Passive,
+        Boss
+    }
 }
